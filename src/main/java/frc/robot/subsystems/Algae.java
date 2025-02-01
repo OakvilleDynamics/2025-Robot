665e6790111b02f4c8e5e094fba730ba0b52b9da
Damir Lipovac <LipovacD93@gmail.com>
2025-02-01 11:00:52 -0600

    Got rid of useless code

--- src/main/java/frc/robot/subsystems/Algae.java
+++ src/main/java/frc/robot/subsystems/Algae.java
@@ -6,21 +6,22 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MechanismConstants;
 
 public class Algae extends SubsystemBase {
   private final SparkFlex AlgaeHinge =
       new SparkFlex(MechanismConstants.ALGAE_HINGE, SparkLowLevel.MotorType.kBrushless);
-  private final SparkFlex AlgaeIntake1 =
-      new SparkFlex(MechanismConstants.ALGAE_INTAKE_1, SparkLowLevel.MotorType.kBrushless);
-  private final SparkFlex AlgaeIntake2 =
-      new SparkFlex(MechanismConstants.ALGAE_INTAKE_2, SparkLowLevel.MotorType.kBrushless);
+  private final SparkMax AlgaeIntake1 =
+      new SparkMax(MechanismConstants.ALGAE_INTAKE_1, SparkLowLevel.MotorType.kBrushless);
+  private final SparkMax AlgaeIntake2 =
+      new SparkMax(MechanismConstants.ALGAE_INTAKE_2, SparkLowLevel.MotorType.kBrushless);
 
   /* public Algea() {
-    AlgeaHinge.setInverted(MechanismConstants.ALGEA_HINGE_INVERTED);
-    AlgeaIntake1.setInverted(MechanismConstants.ALGEA_INTAKE_1_INVERTED);
-    AlgeaIntake2.setInverted(MechanismConstants.ALGEA_INTAKE_2_INVERTED);
+    AlgeaHinge.setInverted(MechanismConstants.ALGAE_HINGE_INVERTED);
+    AlgeaIntake1.setInverted(MechanismConstants.ALGAE_INTAKE_1_INVERTED);
+    AlgeaIntake2.setInverted(MechanismConstants.ALGAE_INTAKE_2_INVERTED);
   } */
 
   public void intakeAlgea() {
