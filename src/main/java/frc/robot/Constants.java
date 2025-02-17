84c4b6850ed3e0cb6b517d60d3fdb58b0bdd8246
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2025-02-17 09:37:25 -0600

    Add field layouts and boundaries
    Modified from Ironclad 5847.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
@@ -40,6 +45,7 @@ public final class Constants {
     public static final double kMAX_AUTO_SPEED = 0.5;
   }
 
+  /** All driver and co-driver controls and settings */
   public static class OperatorConstants {
 
     // Joystick Deadband
@@ -49,7 +55,113 @@ public final class Constants {
     public static final double kTURN_CONSTANT = 6;
   }
 
+  /** Hardware specific constants, such as motor assignments, values, and */
   public static class HardwareConstants {
     public static final int kREV_PDH_ID = 0;
+  }
+
+  /**
+   * Field specific locations of everything, Reef, Coral station, barge, etc. Modified from Ironclad
+   * 2025
+   * https://gitlab.com/ironclad_code/ironclad-2025/-/blob/main/src/main/java/frc/robot/Constants.java?ref_type=heads
+   */
+  public static class FieldConstants {
+    public static final AprilTagFieldLayout fieldLayout =
+        AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+    public static final double fieldLength = Units.inchesToMeters(690.876);
+    public static final double fieldWidth = Units.inchesToMeters(317);
+    // Measured from the inside of starting line
+    public static final double startingLineX = Units.inchesToMeters(299.438);
+
+    public static class Processor {
+      public static final Pose2d centerFace =
+          new Pose2d(Units.inchesToMeters(235.726), 0, Rotation2d.fromDegrees(90));
+    }
+
+    public static class Barge {
+      public static final Translation2d farCage =
+          new Translation2d(Units.inchesToMeters(345.428), Units.inchesToMeters(286.779));
+      public static final Translation2d middleCage =
+          new Translation2d(Units.inchesToMeters(345.428), Units.inchesToMeters(242.855));
+      public static final Translation2d closeCage =
+          new Translation2d(Units.inchesToMeters(345.428), Units.inchesToMeters(199.947));
+
+      // Measured from floor to bottom of cage
+      public static final double deepHeight = Units.inchesToMeters(3.125);
+      public static final double shallowHeight = Units.inchesToMeters(30.125);
+    }
+
+    public static class CoralStation {
+      public static final Pose2d leftCenterFace =
+          new Pose2d(
+              Units.inchesToMeters(33.526),
+              Units.inchesToMeters(291.176),
+              Rotation2d.fromDegrees(90 - 144.011));
+      public static final Pose2d rightCenterFace =
+          new Pose2d(
+              Units.inchesToMeters(33.526),
+              Units.inchesToMeters(25.824),
+              Rotation2d.fromDegrees(144.011 - 90));
+    }
+
+    public static class Reef {
+      public static final Translation2d center =
+          new Translation2d(Units.inchesToMeters(176.746), Units.inchesToMeters(158.501));
+      // Side of the reef to the inside of the reef zone line
+      public static final double faceToZoneLine = Units.inchesToMeters(12);
+
+      public static final Pose2d[] centerFaces =
+          new Pose2d[] {
+            new Pose2d(
+                Units.inchesToMeters(144.003),
+                Units.inchesToMeters(158.500),
+                Rotation2d.fromDegrees(180)),
+            new Pose2d(
+                Units.inchesToMeters(160.373),
+                Units.inchesToMeters(186.857),
+                Rotation2d.fromDegrees(120)),
+            new Pose2d(
+                Units.inchesToMeters(193.116),
+                Units.inchesToMeters(186.858),
+                Rotation2d.fromDegrees(60)),
+            new Pose2d(
+                Units.inchesToMeters(209.489),
+                Units.inchesToMeters(158.502),
+                Rotation2d.fromDegrees(0)),
+            new Pose2d(
+                Units.inchesToMeters(193.118),
+                Units.inchesToMeters(130.145),
+                Rotation2d.fromDegrees(-60)),
+            new Pose2d(
+                Units.inchesToMeters(160.375),
+                Units.inchesToMeters(130.144),
+                Rotation2d.fromDegrees(-120))
+          };
+    }
+  }
+
+  public static class StagingPositions {
+    // Measured from the center of the ice cream
+    public static final Pose2d leftIceCream =
+        new Pose2d(Units.inchesToMeters(48), Units.inchesToMeters(230.5), new Rotation2d());
+    public static final Pose2d middleIceCream =
+        new Pose2d(Units.inchesToMeters(48), Units.inchesToMeters(158.5), new Rotation2d());
+    public static final Pose2d rightIceCream =
+        new Pose2d(Units.inchesToMeters(48), Units.inchesToMeters(86.5), new Rotation2d());
+  }
+
+  public enum ReefHeight {
+    L4(Units.inchesToMeters(72), -90),
+    L3(Units.inchesToMeters(47.625), -35),
+    L2(Units.inchesToMeters(31.875), -35),
+    L1(Units.inchesToMeters(18), 0);
+
+    ReefHeight(double height, double pitch) {
+      this.height = height;
+      this.pitch = pitch; // in degrees
+    }
+
+    public final double height;
+    public final double pitch;
   }
 }
