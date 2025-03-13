74dc07dbac7a02c2f348248b9b8820cd0cf7dd6c
OakvilleDynamicsProgrammer <159196208+OakvilleDynamicsProgrammer@users.noreply.github.com>
2025-03-13 13:57:34 -0500

    Update at 'Thu Mar 13 13:57:32 CDT 2025'

--- src/main/java/frc/robot/commands/AlgaeCommand.java
+++ src/main/java/frc/robot/commands/AlgaeCommand.java
@@ -33,7 +33,7 @@ public class AlgaeCommand extends Command {
     } else if (algaeJoystick.getRawButton(6)) {
       m_AlgaeSubsystem.scoreAlgae();
       System.out.println("Scoring Algae");
-    /* } else if (algaeJoystick.getRawButton(10)) {
+      /* } else if (algaeJoystick.getRawButton(10)) {
       m_AlgaeSubsystem.shootAlgae(); */
       System.out.println("Shooting Algae");
     } else if (algaeJoystick.getRawButton(7)) {
