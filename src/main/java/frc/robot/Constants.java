bc852e222d3020db15745f57bbc954ed1f3a9904
OakvilleDynamicsProgrammer <159196208+OakvilleDynamicsProgrammer@users.noreply.github.com>
2025-03-13 13:26:11 -0500

    Update at 'Thu Mar 13 13:26:10 CDT 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -98,7 +98,7 @@ public final class Constants {
     public static final int kPositionConversionFactor = 360;
     public static final int kVelocityConversionFactor = 1000;
 
-    public static final double kP = 10;
+    public static final double kP = 0;
     public static final double kI = 0;
     public static final double kD = 0;
 
